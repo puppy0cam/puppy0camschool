8b8176f4a1e5f648480b0a20c5b5a8879b718fdc
puppy0cam <7194673+puppy0cam@users.noreply.github.com>
2017-08-17 10:15:14 +1000

    Add files via upload

---
+++ directory.js
@@ -0,0 +1,123 @@
+function wrapper(context){
+    context.pageDirectory = {
+        "BaseURI": function(place) {
+            return new URL("file://eqgbn1699002/chawk37$/My%20Documents/Cert%202%20IDMT/Assessment%20tasks%20&%20Activities/2017/5.%20Digital%20Media/www/"+place);
+        },
+        "page": {
+            "Home": {
+                "index": "#"
+            },
+            "Disclaimer": {
+                "index": "#"
+            },
+            "About": {
+                "index": "#"
+            },
+            "media": {
+                "index": "#",
+                "image1before": "#",
+                "image2before": "#",
+                "image3before": "#",
+                "image1after": "#",
+                "image2after": "#",
+                "image3after": "#"
+            }
+        },
+        "content": {
+            "Home": {
+                "index": "\r\n        <h1>PhotoShop Powertool</h1>\r\n        <table align=\"center\">\r\n            <tr style=\"text-align: inherit; font-size: 20px;\">\r\n            <th style=\"text-align: inherit;\">Before</th>\r\n            <th style=\"text-align: inherit; width:480px;\">Process</th> \r\n            <th style=\"text-align: inherit;\">After</th>\r\n            </tr>\r\n            <tr style=\"text-align: inherit;\">\r\n            <td style=\"text-align: inherit;\"><b><br>Image 1</b></img></td>\r\n            <td style=\"text-align: inherit;\"><b><br>Image 1</b></img></td>\r\n            <td style=\"text-align: inherit;\"><b><br>Image 1</b></img></td>\r\n            </tr>\r\n            <tr style=\"text-align: inherit;\">\r\n            <td style=\"text-align: inherit;\"><img src=\"media/image1before.JPG\" width=\"240px\"></img></td>\r\n            <td width=\"480px\" style=\"text-align: inherit; width: 480px;\">What took place in this image was: first I did...</td> \r\n            <td style=\"text-align: inherit;\"><img src=\"media/image1after.JPG\" width=\"240px\"></img></td>\r\n            </tr>\r\n            <tr style=\"text-align: inherit;\">\r\n            <td style=\"text-align: inherit;\"><b><br>Image 2</b></img></td>\r\n            <td style=\"text-align: inherit;\"><b><br>Image 2</b></img></td>\r\n            <td style=\"text-align: inherit;\"><b><br>Image 2</b></img></td>\r\n            </tr>\r\n            <tr style=\"text-align: inherit;\">\r\n            <td style=\"text-align: inherit;\"><img src=\"media/image2before.JPG\" width=\"240px\"></img></td>\r\n            <td width=\"480px\" style=\"text-align: inherit; width: 480px;\">What took place in this image was: first I did...</td> \r\n            <td style=\"text-align: inherit;\"><img src=\"media/image2after.JPG\" width=\"240px\"></img></td>\r\n            </tr>\r\n            <tr style=\"text-align: inherit;\">\r\n            <td style=\"text-align: inherit;\"><b><br>Image 3</b></img></td>\r\n            <td style=\"text-align: inherit;\"><b><br>Image 3</b></img></td>\r\n            <td style=\"text-align: inherit;\"><b><br>Image 3</b></img></td>\r\n            </tr>\r\n            <tr style=\"text-align: inherit;\">\r\n            <td style=\"text-align: inherit;\"><img src=\"media/image3before.JPG\" width=\"240px\"></img></td>\r\n            <td width=\"480px\" style=\"text-align: inherit; width: 480px;\">What took place in this image was: first I did...</td> \r\n            <td style=\"text-align: inherit;\"><img src=\"media/image3after.JPG\" width=\"240px\"></img></td>\r\n            </tr>\r\n        </table>\r\n        <br>"
+            },
+            "Disclaimer": {
+                "index": "\r\n        <h1>Disclaimer</h1>\r\n        <p>The information contained in this website is for educational purposes only.</p>\r\n        <p>The information provided in this website does not in any way, shape or form, relate to any existing business.</p>\r\n        <p>We make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</p>\r\n        <p>In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.</p>\r\n        <br>"
+            },
+            "About": {
+                "index": "\r\n        <h1>About Us</h1>\r\n        <p>Calamvale Community College is a P-12 Independent State School established in 2001.</p>\r\n        <p>It is divided into two sub-schools – Junior and Secondary.</p>\r\n        <p>Each has a separate principal and teaching staff, along with a college principal to lead the rest of the school</p>"
+            },
+            "media": {
+                "image1before": "\r\n        <img src=\"media/image1before.JPG\" href=\"#\" onclick=\"pageDirectory.openPage('Home');\"></img>",
+                "image2before": "\r\n        <img src=\"media/image2before.JPG\" href=\"#\" onclick=\"pageDirectory.openPage('Home');\"></img>",
+                "image3before": "\r\n        <img src=\"media/image3before.JPG\" href=\"#\" onclick=\"pageDirectory.openPage('Home');\"></img>",
+                "image1after": "\r\n        <img src=\"media/image1after.JPG\" href=\"#\" onclick=\"pageDirectory.openPage('Home');\"></img>",
+                "image2after": "\r\n        <img src=\"media/image2after.JPG\" href=\"#\" onclick=\"pageDirectory.openPage('Home');\"></img>",
+                "image3after": "\r\n        <img src=\"media/image3after.JPG\" href=\"#\" onclick=\"pageDirectory.openPage('Home');\"></img>"
+            }
+        },
+        "currentPage": "Home",
+        "createNavBar": function(page) {
+            var page = page || "Home";
+            var data = document.createElement("ul");
+            for (var thisPage in context.pageDirectory.page) {
+                var i = 0;
+                var elem = document.createElement("li");
+                for (var temp1456 in context.pageDirectory.page[thisPage]) {
+                    i++
+                }
+                if(i>1) {
+                    var ul = document.createElement("ul");
+                    ul.setAttribute("class","dropdown-menu");
+                    elem.setAttribute("class","dropdown");
+                }
+                for (var subItem in context.pageDirectory.page[thisPage]) {
+                    var textArea = document.createElement("a");
+                    textArea.innerText = thisPage;
+                    textArea.setAttribute("href","#");
+                    textArea.setAttribute("onclick","pageDirectory.openPage('"+thisPage+"/"+subItem+"');");
+                    if(subItem === "index") {
+                        textArea = document.createElement("a");
+                        textArea.innerText = thisPage;
+                        if(i>1) {
+                            textArea.setAttribute("class", "dropdown-toggle");
+                            textArea.setAttribute("data-toggle", "dropdown");
+                            textArea.setAttribute("role","button");
+                            textArea.setAttribute("aria-haspopup","true");
+                            textArea.setAttribute("aria-expanded","false");
+                            textArea.setAttribute("href","#");
+                            var span = document.createElement("span");
+                            span.setAttribute("class","caret");
+                            textArea.appendChild(span);
+                            elem.appendChild(textArea);
+                        }
+                    }
+                    if(i>1 && subItem !== "index") {
+                        textArea.innerText = subItem;
+                        textArea.setAttribute("href","#");
+                        textArea.setAttribute("onclick","pageDirectory.openPage('"+thisPage+"/"+subItem+"');");
+                        var li = document.createElement("li");
+                        li.appendChild(textArea);
+                        ul.appendChild(li);
+                    }
+                    if(ul) {
+                        elem.appendChild(ul);
+                        if(thisPage === page) {
+                            //elem.setAttribute("class","active");
+                            elem.setAttribute("style","background-image: linear-gradient(to bottom,#A52A2A,#A52A2A,#A52A2A,#A52A2A,teal);")
+                        }
+                    } else {
+                        textArea.setAttribute("href","#");
+                        textArea.setAttribute("onclick","pageDirectory.openPage('"+thisPage+"/"+subItem+"');");
+                        elem.appendChild(textArea);
+                        if(thisPage === page) {
+                            //elem.setAttribute("class","active");
+                            elem.setAttribute("style","background-image: linear-gradient(to bottom,#A52A2A,#A52A2A,#A52A2A,#A52A2A,teal);")
+                        }
+                    }
+                }
+                data.appendChild(elem);
+            }
+            return data
+        },
+        "openPage": function(page,data) {
+            var page = page.split("/") || ["Home","index"];
+            var data = data || context.pageDirectory.content[page[0]][page[1]];
+            context.pageDirectory.currentPage = page.join("/");
+            context.document.getElementById("navigationBar").innerHTML = context.pageDirectory.createNavBar(page[0]).innerHTML;
+            context.document.getElementById("windowContent").innerHTML = data;
+            console.log("page load");
+        }
+    };
+    window.onload = function(){
+        console.log("loading");
+        pageDirectory.openPage("Home/index");
+    };
+}
+wrapper(window);
